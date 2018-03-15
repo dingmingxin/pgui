9aa748491e3d5e83547a4b020e1ec2a656d733b1
dmx <dingmingxin20@gmail.com>
2018-03-15 10:56:54 +0800

    add src/editor; update CMakeLists.txt

---
+++ src/editor/main.c
@@ -0,0 +1,27 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  main.c
+ *
+ *    Description:  editor entrance
+ *
+ *        Version:  1.0
+ *        Created:  03/15/2018 10:44:52
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  dingmingxin (dmx), dingmingxin20@gmail.com
+ *   Organization:  coding
+ *
+ * =====================================================================================
+ */
+#include <stdlib.h>
+#include <stdio.h>
+int 
+main(int argc, const char *argv[])
+{
+	
+	printf("hello world\n");
+	return 0;
+}
+
